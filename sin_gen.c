4571cd8b5f7a651b53dbfce8c55aab25d63c4624
Wiebe Van Ranst <wiebevr@gmail.com>
2013-03-07 12:37:11 +0100

    Added main structure

--- sin_gen.c
+++ sin_gen.c
@@ -1,10 +1,47 @@
 #include "lcd.h"
 
+char g_flags;
+#define TIMER_FLAG 0x01
+#define LED_FLAG 0x02
+#define UART_FLAG 0x04
+
+void update_timer()
+{
+    // TODO: Implement 
+}
+
+void update_lcd()
+{
+    // TODO: Implement 
+}
+
+void update_uart()
+{
+    // TODO: Implement 
+}
+
 
 void main(void)
 {
     while (1)
     {
+        if (g_flags & TIMER_FLAG)
+        {
+            update_timer();
 
+            g_flags &= ~TIMER_FLAG;
+        }
+        if (g_flags & LCD_FLAG)
+        {
+            update_lcd();
+
+            g_flags &= ~LCD_FLAG;
+        }
+        if (g_flags & UART_FLAG)
+        {
+            update_uart();
+
+            g_flags &= ~UART_FLAG;
+        }
     }
 }

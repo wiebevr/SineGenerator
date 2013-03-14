bb4252040393d05f1097ab9488643107ae38f605
Ken Peersman <Ken.peersman@hotmail.com>
2013-03-14 12:42:04 +0100

    Spanningen op lcd

--- sin_gen.c
+++ sin_gen.c
@@ -1,53 +1,48 @@
-#include "lcd.h"
 #include "reg832.h"
+#include "lcd.h"
+#include <stdio.h>
 
 char g_flags;
 #define TIMER_FLAG 0x01
 #define LCD_FLAG 0x02
 #define UART_FLAG 0x04
+#define ADC_FLAG 0x08
+static unsigned short g_adc_value = 0;
 
-char read_adc();
+unsigned short read_adc();
 void init_adc();
-
-void update_timer()
-{
-    // TODO: Implement 
-}
-
-void update_lcd()
-{
-    // TODO: Implement 
-}
-
-void update_uart()
-{
-    // TODO: Implement 
-}
-
+void update_timer();
+void update_lcd();
+void update_uart();
+void update_adc();
 
 void main(void)
 {
 	init_adc();
+	InitializeLCD();
     while (1)
     {
-		P2 = read_adc();
+		g_adc_value = read_adc();
+		update_lcd();
         if (g_flags & TIMER_FLAG)
         {
             update_timer();
-
             g_flags &= ~TIMER_FLAG;
         }
         if (g_flags & LCD_FLAG)
         {
             update_lcd();
-
             g_flags &= ~LCD_FLAG;
         }
         if (g_flags & UART_FLAG)
         {
             update_uart();
-
             g_flags &= ~UART_FLAG;
+        }
+		if (g_flags & ADC_FLAG)
+        {
+            update_adc();
+            g_flags &= ~ADC_FLAG;
         }
     }
 }
@@ -57,10 +52,38 @@ void init_adc()
 	ADCCON1 = 0xDC;   //11011100
 	ADCCON2 = 0x27;	  //00000111
 }
-char read_adc()
+unsigned short read_adc()
 {
-	char value;
-	// Return 8 most significant bits
-	value = ADCDATAH << 4;
-	return value | ((ADCDATAL & 0xf0) >> 4);
+	// Return 12 most significant bits
+	return ((ADCDATAH & 0x0f) << 8) | ADCDATAL;
 }
+
+void update_timer()
+{
+    // TODO: Implement 
+}
+
+void update_lcd()
+{
+	char buffer[6] = "00000";
+    ClearScreen();
+	WriteStringAtPos(0,0,"ADC:");
+	sprintf(buffer, "%4.4d", (unsigned short)(g_adc_value*1.221));
+	buffer[2] = buffer[1];buffer[3] = buffer[2];
+	buffer[4] = 'V'; buffer[1] = '.';
+	WriteStringAtPos(0,10,buffer);
+	WriteStringAtPos(1,0,"Sinus:");
+	WriteStringAtPos(1,10,buffer);
+}
+
+void update_uart()
+{
+    // TODO: Implement 
+}
+
+void update_adc()
+{
+	//moving average nog doen
+    g_adc_value = read_adc(); 
+}
+

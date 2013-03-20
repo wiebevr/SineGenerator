a66a05596f28499f046a9539e5ab67a99d1810dd
Wiebe Van Ranst <wiebevr@gmail.com>
2013-03-20 21:49:01 +0100

    Foutje in dds waarde opgelost (hoop ik)

--- sine_values.py
+++ sine_values.py
@@ -6,14 +6,14 @@ def main(argv):
     print "const unsigned char dac_high[" + str(num_values) + "] ="
     print "{"
     for i in range(num_values):
-        print "    " + str(int(round(m.sin( i * 2 * m.pi / num_values) * 2 + 2))) +\
+        print "    " + str(int(round((1+m.sin( i * 2 * m.pi / num_values)) * 7.5 ))) +\
                 ("," if i != num_values - 1 else "")
     print "};"
 
     print "const unsigned char dac_low[" + str(num_values) + "] ="
     print "{"
     for i in range(num_values):
-        print "    " + str(int(round(m.sin( i * 2 * m.pi / num_values) * 2**11 + 2**11)) & 0x00FF ) +\
+        print "    " + str(int(round((1+m.sin( i * 2 * m.pi / num_values)) * (2**12 - 1) / 2.0)) & 0x00FF ) +\
                 ("," if i != num_values - 1 else "")
     print "};"
 
